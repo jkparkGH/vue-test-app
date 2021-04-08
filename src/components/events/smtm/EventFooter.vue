5d9065a529e08c378f21f945f2490b1e8f51a3d3
jake <pjklogs@naver.com>
2021-04-08 16:47:20 +0900

    feat: event EventFooter 컴포넌트 추가

---
+++ src/components/events/smtm/EventFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="event-footer">
+    <img
+      class="event-footer__bg-image"
+      src="@/assets/images/event_smtm/bg_bottom_coins.png"
+      alt=""
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class EventFooter extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+$modules: 'event-footer';
+.#{$modules} {
+  width: 100%;
+  margin-top: 24px;
+  &__bg-image {
+    display: block;
+    width: 100%;
+  }
+}
+</style>

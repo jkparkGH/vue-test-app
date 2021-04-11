3b83169e120bc312eba87a824829018cb0480a6c
jake <pjklogs@naver.com>
2021-04-11 14:11:44 +0900

    feat: home 컴포넌트 컨텐츠 추가

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="main-landing-container">HOME</div>
+  <div class="main-container">
+    <h1>Welcome :D</h1>
+    <h3>
+      <span>Admin</span>
+      <br />
+      Sample
+      <br />
+      Page
+      <br />
+      <img src="@/assets/images/icon.png" alt="" />
+    </h3>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class MainLanding extends Vue {}
+export default class HomeComponent extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+$modules: 'main-container';
+.main-container {
+  padding: 100px;
+  @media (max-width: 480px) {
+    padding: 48px 24px;
+  }
+  h1 {
+    padding-bottom: 16px;
+  }
+  img {
+    padding-top: 10px;
+  }
+}
+</style>

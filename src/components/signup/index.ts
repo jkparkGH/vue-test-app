dcbcdae7861f4bf8bcbf5a28054302a4854fd292
jake <pjklogs@naver.com>
2021-04-11 14:46:13 +0900

    feat: 회원가입 페이지 Index.vue 컴포넌트 바인딩

---
+++ src/components/signup/index.ts
@@ -0,0 +1,8 @@
+export { default as LoginInfo } from './LoginInfo.vue';
+export { default as GenderSelection } from './GenderSelection.vue';
+export { default as BirthDayInfo } from './BirthDayInfo.vue';
+export { default as PhoneNumber } from './PhoneNumber.vue';
+export { default as ToggleAgreement } from './ToggleAgreement.vue';
+export { default as SignupSubmitButton } from './SignupSubmitButton.vue';
+
+export { default as SignupComponent } from './Index.vue';
--- src/components/signup/index.vue
+++ src/components/signup/index.vue
@@ -1,10 +1,99 @@
 <template>
-  <div class="signup-container">SIGNUP</div>
+  <main class="signup-container">
+    <h1 class="signup-container__title">회원가입</h1>
+    <form @submit.prevent="submitSignUp">
+      <LoginInfo :user-id.sync="userInfo.id" :user-password.sync="userInfo.password" />
+      <GenderSelection :user-gender.sync="userInfo.gender" />
+      <BirthDayInfo
+        :birthday_year.sync="userInfo.birthday.year"
+        :birthday_month.sync="userInfo.birthday.month"
+        :birthday_day.sync="userInfo.birthday.day"
+      />
+      <PhoneNumber
+        :tel_company_number.sync="userInfo.phone_number.tel_company_number"
+        :last_number.sync="userInfo.phone_number.last_number"
+        :is-phone-number-confirm.sync="isPhoneNumberConfirm"
+      />
+      <ToggleAgreement :agreement.sync="userInfo.agreement" />
+      <SignupSubmitButton />
+    </form>
+  </main>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator';
+import {
+  LoginInfo,
+  GenderSelection,
+  BirthDayInfo,
+  PhoneNumber,
+  ToggleAgreement,
+  SignupSubmitButton,
+} from './';
 
-@Component
-export default class SignupComponent extends Vue {}
+@Component({
+  components: {
+    LoginInfo,
+    GenderSelection,
+    BirthDayInfo,
+    PhoneNumber,
+    ToggleAgreement,
+    SignupSubmitButton,
+  },
+})
+export default class SignupComponent extends Vue {
+  userInfo = {
+    id: '',
+    password: '',
+    gender: '',
+    birthday: {
+      year: '',
+      month: '',
+      day: '',
+    },
+    phone_number: {
+      tel_company_number: '',
+      last_number: '',
+    },
+    agreement: false,
+  };
+
+  isPhoneNumberConfirm = false;
+
+  submitSignUp() {
+    // Clinet Validation
+    alert('가입하기!');
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+$modules: 'signup-container';
+.#{$modules} {
+  padding: 100px;
+  @media (max-width: 480px) {
+    padding: 48px 24px 24px;
+  }
+
+  &__title {
+    font-size: 24px;
+    margin-bottom: 48px;
+    position: relative;
+    color: #555;
+    &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -5px;
+      width: 90px;
+      height: 5px;
+      border-radius: 4px;
+      background-color: #ffeb00;
+    }
+  }
+
+  section + section {
+    margin-top: 36px;
+  }
+}
+</style>

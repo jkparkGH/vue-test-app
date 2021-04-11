b50f65150848ae6cdfe6c05bf3e969adf81a4d2a
jake <pjklogs@naver.com>
2021-04-11 14:09:49 +0900

    feat: moveBack 컴포넌트 추가

---
+++ src/components/common/MoveBackButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button class="move-back" v-show="isShowMoveBack" @click="moveBack">←</button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component
+export default class MoveBackButton extends Vue {
+  @Prop({
+    required: false,
+    type: Boolean,
+    default: true,
+  })
+  readonly isShowMoveBack!: boolean;
+
+  moveBack() {
+    this.$router.back();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$modules: 'move-back';
+.#{$modules} {
+  position: fixed;
+  z-index: 10;
+  bottom: 20px;
+  right: 20px;
+  width: 24px;
+  height: 24px;
+  line-height: 16px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #000;
+  border: 2px solid #fff;
+  color: yellow;
+  font-weight: 600;
+  color: #fff;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
+}
+</style>
